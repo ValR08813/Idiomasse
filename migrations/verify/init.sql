57809644af50e2b539859829aa39550595663596
ValR08813 <valentin.deschins@gmail.com>
2022-06-28 19:48:55 +0200

    script start + run + router bones

--- migrations/verify/init.sql
+++ migrations/verify/init.sql
@@ -6,5 +6,5 @@ SELECT * FROM user_know_word WHERE false;
 SELECT * FROM word WHERE false;
 SELECT * FROM "type" WHERE false;
 SELECT * FROM "language" WHERE false;
-SELECT * FROM user WHERE false;
+SELECT * FROM "user" WHERE false;
 ROLLBACK;
